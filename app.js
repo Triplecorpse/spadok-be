49456e2caf1ccbc5a07750052aaf46dce1748934
Eldar Khaitov <Eldar_Khaitov@epam.com>
2016-07-25 02:24:17 +0300

    mongoose added

--- app.js
+++ app.js
@@ -8,38 +8,38 @@ const http         = require('http'),
       env          = process.env,
       mongoose     = require('mongoose');
 
-mongoose.connect(env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("we're connected!");
-});
-
-var kittySchema = mongoose.Schema({
-    name: String
-});
-// NOTE: methods must be added to the schema before compiling it with mongoose.model()
-kittySchema.methods.speak = function () {
-    var greeting = this.name
-        ? "Meow name is " + this.name
-        : "I don't have a name";
-    console.log(greeting);
-}
-var Kitten = mongoose.model('Kitten', kittySchema);
-var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
-
-fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
-});
-
-Kitten.find(function (err, kittens) {
-    if (err) return console.error("ERROR", err);
-    console.log(kittens);
-})
+// mongoose.connect(env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/test');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log("we're connected!");
+// });
+//
+// var kittySchema = mongoose.Schema({
+//     name: String
+// });
+// // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+// kittySchema.methods.speak = function () {
+//     var greeting = this.name
+//         ? "Meow name is " + this.name
+//         : "I don't have a name";
+//     console.log(greeting);
+// }
+// var Kitten = mongoose.model('Kitten', kittySchema);
+// var silence = new Kitten({ name: 'Silence' });
+// console.log(silence.name); // 'Silence'
+// var fluffy = new Kitten({ name: 'fluffy' });
+// fluffy.speak(); // "Meow name is fluffy"
+//
+// fluffy.save(function (err, fluffy) {
+//     if (err) return console.error(err);
+//     fluffy.speak();
+// });
+//
+// Kitten.find(function (err, kittens) {
+//     if (err) return console.error("ERROR", err);
+//     console.log(kittens);
+// })
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
