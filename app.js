30979df7f76c8c943218513b909773f397ef477b
Eldar Khaitov <Eldar_Khaitov@epam.com>
2016-07-25 16:52:13 +0300

    mongoose added

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const http         = require('http'),
     sysInfo      = require('./utils/sys-info'),
     env          = process.env,
     express = require('express'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
 
 // let server = http.createServer(function (req, res) {
 //     let url = req.url;
@@ -40,7 +41,7 @@ const http         = require('http'),
 //         });
 //     }
 // });
-
+//
 // server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
 //     console.log(`Application worker ${process.pid} started...`);
 // });
@@ -55,38 +56,38 @@ const http         = require('http'),
 //       env          = process.env;
 //       // mongoose     = require('mongoose');
 //
-// // mongoose.connect(env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/test');
-// // var db = mongoose.connection;
-// // db.on('error', console.error.bind(console, 'connection error:'));
-// // db.once('open', function() {
-// //     console.log("we're connected!");
-// // });
-// //
-// // var kittySchema = mongoose.Schema({
-// //     name: String
-// // });
-// // // NOTE: methods must be added to the schema before compiling it with mongoose.model()
-// // kittySchema.methods.speak = function () {
-// //     var greeting = this.name
-// //         ? "Meow name is " + this.name
-// //         : "I don't have a name";
-// //     console.log(greeting);
-// // }
-// // var Kitten = mongoose.model('Kitten', kittySchema);
-// // var silence = new Kitten({ name: 'Silence' });
-// // console.log(silence.name); // 'Silence'
-// // var fluffy = new Kitten({ name: 'fluffy' });
-// // fluffy.speak(); // "Meow name is fluffy"
-// //
-// // fluffy.save(function (err, fluffy) {
-// //     if (err) return console.error(err);
-// //     fluffy.speak();
-// // });
-// //
-// // Kitten.find(function (err, kittens) {
-// //     if (err) return console.error("ERROR", err);
-// //     console.log(kittens);
-// // })
+mongoose.connect(env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("we're connected!");
+});
+
+var kittySchema = mongoose.Schema({
+    name: String
+});
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function () {
+    var greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+    console.log(greeting);
+}
+var Kitten = mongoose.model('Kitten', kittySchema);
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+});
+
+Kitten.find(function (err, kittens) {
+    if (err) return console.error("ERROR", err);
+    console.log(kittens);
+})
 //
 app.get('/', function (req, res) {
   // res.send('Hello World!');
@@ -102,9 +103,13 @@ app.get('/health', function (req, res) {
     res.end('1');
 });
 
-app.get('/health2', function (req, res) {
-    res.writeHead(200);
-    res.end('Healthy');
+app.get('/kittens', function (req, res) {
+    Kitten.find(function (err, kittens) {
+        if (err) return console.error("ERROR", err);
+        console.log(kittens);
+        res.writeHead(200);
+        res.end(JSON.stringify(kittens));
+    })
 });
 
 app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', () => {
