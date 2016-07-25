3b718f892a10afd5e5a50e075789200264905824
Eldar Khaitov <Eldar_Khaitov@epam.com>
2016-07-25 11:53:45 +0300

    mongoose removed

--- app.js
+++ app.js
@@ -5,42 +5,43 @@ const http         = require('http'),
     sysInfo      = require('./utils/sys-info'),
     env          = process.env;
 
-let server = http.createServer(function (req, res) {
-    let url = req.url;
-    if (url == '/') {
-        url += 'index.html';
-    }
+// let server = http.createServer(function (req, res) {
+//     let url = req.url;
+//     if (url == '/') {
+//         fs.readFile();
+//         url += 'index.html';
+//     }
+//
+//     // IMPORTANT: Your application HAS to respond to GET /health with status 200
+//     //            for OpenShift health monitoring
+//
+//     if (url == '/health') {
+//         res.writeHead(200);
+//         res.end();
+//     } else if (url == '/info/gen' || url == '/info/poll') {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.setHeader('Cache-Control', 'no-cache, no-store');
+//         res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+//     } else {
+//         fs.readFile('./static' + url, function (err, data) {
+//             if (err) {
+//                 res.writeHead(404);
+//                 res.end('Not found');
+//             } else {
+//                 let ext = path.extname(url).slice(1);
+//                 res.setHeader('Content-Type', contentTypes[ext]);
+//                 if (ext === 'html') {
+//                     res.setHeader('Cache-Control', 'no-cache, no-store');
+//                 }
+//                 res.end(data);
+//             }
+//         });
+//     }
+// });
 
-    // IMPORTANT: Your application HAS to respond to GET /health with status 200
-    //            for OpenShift health monitoring
-
-    if (url == '/health') {
-        res.writeHead(200);
-        res.end();
-    } else if (url == '/info/gen' || url == '/info/poll') {
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Cache-Control', 'no-cache, no-store');
-        res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-    } else {
-        fs.readFile('./static' + url, function (err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Not found');
-            } else {
-                let ext = path.extname(url).slice(1);
-                res.setHeader('Content-Type', contentTypes[ext]);
-                if (ext === 'html') {
-                    res.setHeader('Cache-Control', 'no-cache, no-store');
-                }
-                res.end(data);
-            }
-        });
-    }
-});
-
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-    console.log(`Application worker ${process.pid} started...`);
-});
+// server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+//     console.log(`Application worker ${process.pid} started...`);
+// });
 
 // const http         = require('http'),
 //       fs           = require('fs'),
@@ -85,21 +86,21 @@ server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
 // //     console.log(kittens);
 // // })
 //
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-//
-// app.get('/health', function (req, res) {
-//     res.writeHead(200);
-//     res.end('1');
-// });
-//
-//
-// app.get('/health2', function (req, res) {
-//     res.writeHead(200);
-//     res.end('Healthy');
-// });
-// app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', () => {
-//   // console.log(`Example app listening on port ${env.NODE_PORT || 3000}!`);
-//   console.log(`Application worker ${process.pid} started...`);
-// });
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/health', function (req, res) {
+    res.writeHead(200);
+    res.end('1');
+});
+
+app.get('/health2', function (req, res) {
+    res.writeHead(200);
+    res.end('Healthy');
+});
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', () => {
+  // console.log(`Example app listening on port ${env.NODE_PORT || 3000}!`);
+  console.log(`Application worker ${process.pid} started...`);
+});
