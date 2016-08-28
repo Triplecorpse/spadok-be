987e0ca84473bcbc3ca4b1dd862f61b08caab4ba
Eldar Khaitov <eldar.khaitov@gmail.com>
2016-08-28 16:13:03 +0300

    Project page completed bug fix

--- static/src/index.js
+++ static/src/index.js
@@ -49,6 +49,10 @@ angular.module('app').controller('appController', ['$window', '$scope', 'dataSer
         }
     };
 
+    $scope.$on('$locationChangeSuccess', () => {
+        window.scrollTo(0, 0);
+    });
+
     String.prototype.splice = function(start, delCount, newSubStr) {
         return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
     };
