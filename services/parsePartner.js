2f5c5331bcf249680ea136794f549e79cc3ac376
Eldar Khaitov <eldar.khaitov@gmail.com>
2016-09-14 14:51:06 +0300

    partner - fix3

--- services/parsePartner.js
+++ services/parsePartner.js
@@ -10,19 +10,6 @@ module.exports = (data) => {
     for(let key in data) {
         if(data.hasOwnProperty(key)) {
             if(Array.isArray(data[key])) {
-                data[key] = data[key].map((element) => {
-                    if(typeof element === 'string') {
-                        element = element.replace(/(>)/g, '&gt;').replace(/(<)/g, '&lt;');
-                    }
-                    return element;
-                });
-            }
-        }
-    }
-
-    for(let key in data) {
-        if(data.hasOwnProperty(key)) {
-            if(Array.isArray(data[key])) {
                 data[key] = data[key].filter((element) => {
                     return Boolean(element);
                 });
