a94d695013ec525ee1c03ed8eb81a19f35b26f71
Eldar Khaitov <eldar.khaitov@gmail.com>
2016-10-20 21:12:39 +0300

    Translation added fix4

--- services/parsePage.js
+++ services/parsePage.js
@@ -3,17 +3,18 @@ module.exports = (data) => {
         if (data.hasOwnProperty(key)) {
             for (let i in data[key]) {
 
-                if (key === 'stats') {
-                    data[key][i] = data[key][i].replace(/(<strong>)/g, '||strong||').replace(/(<\/strong>)/g, '||/strong||');
-                }
+                if (data[key].hasOwnProperty(i)) {
+                    if (key === 'stats') {
+                        data[key][i] = data[key][i].replace(/(<strong>)/g, '||strong||').replace(/(<\/strong>)/g, '||/strong||');
+                    }
 
-                if (typeof data[key][i] === 'string') {
-                    let asd = data[key][i].replace(/(>)/g, '&gt;').replace(/(<)/g, '&lt;');
-                    data[key][i] = asd;
-                }
+                    if (typeof data[key][i] === 'string') {
+                        data[key][i] = data[key][i].replace(/(>)/g, '&gt;').replace(/(<)/g, '&lt;');
+                    }
 
-                if (key === 'stats') {
-                    data[key][i] = data[key][i].split('||strong||').join('<strong>').split('||/strong||').join('</strong>');
+                    if (key === 'stats') {
+                        data[key][i] = data[key][i].split('||strong||').join('<strong>').split('||/strong||').join('</strong>');
+                    }
                 }
 
             }
