b0a84a64df9e280dfeda46f072fbc9f8b24f67bd
Eldar Khaitov <eldar.khaitov@gmail.com>
2016-10-20 21:32:58 +0300

    Translation added fix6

--- services/parsePage.js
+++ services/parsePage.js
@@ -1,17 +1,22 @@
 module.exports = (data) => {
-    console.log("page data", data);
+    //console.log("page data", data);
     for (let key in data) {
-        console.log("key of page", key);
+        //console.log("key of page", key);
+
+        if (typeof data[key] === 'string') {
+            data[key] = data[key].replace(/(>)/g, '&gt;').replace(/(<)/g, '&lt;');
+        }
+
         if (data.hasOwnProperty(key)) {
             for (let i in data[key]) {
 
-                console.log("some enum in " + key + " of page", i);
+                //console.log("some enum in " + key + " of page", i);
                 if (data[key].hasOwnProperty(i)) {
                     if (key === 'stats') {
                         data[key][i] = data[key][i].replace(/(<strong>)/g, '||strong||').replace(/(<\/strong>)/g, '||/strong||');
                     }
 
-                    if (typeof data[key][i] === 'string') {
+                    if ((typeof data[key] === 'object') && (typeof data[key][i] === 'string')) {
                         data[key][i] = data[key][i].replace(/(>)/g, '&gt;').replace(/(<)/g, '&lt;');
                     }
 
